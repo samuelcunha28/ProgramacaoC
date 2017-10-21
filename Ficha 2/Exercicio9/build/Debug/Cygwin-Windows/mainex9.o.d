56a16862748cf5b278c60a234310e3b5d932fa8f
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-21 16:22:09 +0100

    primeiro commit

---
+++ Ficha 2/Exercicio9/build/Debug/Cygwin-Windows/mainex9.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/mainex9.o: mainex9.c
---
+++ Ficha 2/Exercicio9/mainex9.c
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainex9.c
+ * Author: samue
+ *
+ * Created on 21 de Outubro de 2017, 16:11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int numero;
+    
+    puts("Introduza o seu numero: ");
+    scanf("%d", &numero);
+    
+    if (numero % 2 == 0) { // a sua divisao por 2 tem que ter resto 0 para ser par 
+        puts("O numero que foi introduzido e par");
+    } else {
+        puts("O numero que foi introduzido e impar");
+    }
+    return (EXIT_SUCCESS);
+}
+
