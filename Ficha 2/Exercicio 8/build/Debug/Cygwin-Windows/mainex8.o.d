c012e1a0d196756a797f40a511cd86aab9c5c1ce
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-21 16:09:39 +0100

    primeiro commit

---
+++ Ficha 2/Exercicio 8/build/Debug/Cygwin-Windows/mainex8.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/mainex8.o: mainex8.c
---
+++ Ficha 2/Exercicio 8/mainex8.c
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainex8.c
+ * Author: samue
+ *
+ * Created on 21 de Outubro de 2017, 15:13
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int horaentrada, horasaida, minutoentrada, minutosaida, totalentrada, totalsaida, total;
+    
+    puts("Introduza a hora de entrada do empregado: ");
+    scanf("%d", &horaentrada);
+    puts("Introduza o minuto de entrada do empregado: ");
+    scanf("%d", &minutoentrada);
+    puts("Introduza a hora de saida do empregado: ");
+    scanf("%d", &horasaida);
+    puts("Introduza o minuto de saida do empregado: ");
+    scanf("%d", &minutosaida);
+    
+    totalentrada = (horaentrada * 60) + minutoentrada;
+    totalsaida = (horasaida * 60) + minutosaida;
+    total = totalsaida - totalentrada;
+   
+    if (total < 0) {
+        total = (24 * 60) + total;
+    }
+    printf("O total de tempo trabalhado pelo empregado foi de: %d horas e %d minutos\n ", total / 60, total % 60);
+    
+    return (EXIT_SUCCESS);
+}
+
