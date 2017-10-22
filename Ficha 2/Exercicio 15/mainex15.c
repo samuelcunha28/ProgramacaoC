3cd8400068503d976ed0da25f13e3e0cf12c6529
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-22 20:59:46 +0100

    segundo commit

---
+++ Ficha 2/Exercicio 15/mainex15.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainex15.c
+ * Author: samue
+ *
+ * Created on 22 de Outubro de 2017, 18:18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
