403ad4878ff4a25b4e29f69c6b1221c04911315a
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-22 21:28:11 +0100

    primeiro commit incompleto

--- Ficha 2/Exercicio 15/mainex15.c
+++ Ficha 2/Exercicio 15/mainex15.c
@@ -14,11 +14,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define distancia  130
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    int tempopercurso, combustivel, velmedia;
+    
+    puts("Introduza o total de tempo que demorou a fazer o percurso: (em minutos)");
+    scanf("%d", &tempopercurso);
+    
+    velmedia = (distancia * 60) / tempopercurso;
+    printf ("Velocidade media: %.1d\n ", velmedia);
+    
+    if (velmedia <= 60) {
+        combustivel = (5 / distancia) * 60;
+        printf("Consumo medio: %.1d\n ", combustivel);
+        
+    }
+    
     return (EXIT_SUCCESS);
 }
 
