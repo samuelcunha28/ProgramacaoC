78fa419db28295410549d08bd13c859d4b653bca
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-25 21:50:22 +0100

    correcao exercicio

--- Ficha 2/Exercicio 15/mainex15.c
+++ Ficha 2/Exercicio 15/mainex15.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define distancia 130
+#define distanciapercurso 130
 #define alcmax 4
 
 /*
@@ -28,20 +28,20 @@ int main(int argc, char** argv) {
     scanf("%d", &tempopercurso);
 
     tempohoras = tempopercurso / 60; // passando para horas
-    velmedia = distancia / tempohoras; //calculo da velocidade media
+    velmedia = distanciapercurso / tempohoras; //calculo da velocidade media
     printf("A velocidade media e de: %.1lf\n ", velmedia);
 
     if (velmedia <= 60) {
         maxkm = (alcmax * 100) / 5; // regra 3 simples para velocidade media menor ou igual a 60 km/h
-        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        printf("Para velocidades medias menores ou igual a 60 kmh, o maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
         puts("Tem alcance suficiente para parar na ultima bomba para abastecer ");
     } else if (velmedia > 60 && velmedia <= 120) {
         maxkm = (alcmax * 100) / 6; // regra 3 simples para velocidade media maior que 60 e menor ou igual a 120 km/h
-        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        printf("Para velocidades medias maiores que 60 kmh e menores ou iguais a 120 kmh o maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
         puts("Tem alcance suficiente para parar na segunda bomba para abastecer ");
     } else if (velmedia > 120) {
         maxkm = (alcmax * 100) / 7; // regra 3 simples para velocidade media maior que 120 km/h
-        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        printf("Para velocidades medias maiores que 120 kmh, o maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
         puts("Impossivel de chegar à primeira bomba. Infelizmente vai ficar encostado a berma ");
     }
     return (EXIT_SUCCESS);
