200e383b4e8d3e0619a0a43a19f08099f75333f7
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-25 19:08:20 +0100

    finalizacao exercicio

--- Ficha 2/Exercicio 15/mainex15.c
+++ Ficha 2/Exercicio 15/mainex15.c
@@ -15,34 +15,35 @@
 #include <stdlib.h>
 
 #define distancia 130
+#define alcmax 4
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    int tempopercurso;
-    double combustivel, velmedia;
+    int tempopercurso, tempohoras;
+    float maxkm, velmedia;
 
     puts("Introduza o total de tempo que demorou a fazer o percurso: (em minutos)");
     scanf("%d", &tempopercurso);
 
-    velmedia = (distancia * 60) / tempopercurso;
-    printf("Velocidade media: %f\n ", velmedia);
+    tempohoras = tempopercurso / 60; // passando para horas
+    velmedia = distancia / tempohoras; //calculo da velocidade media
+    printf("A velocidade media e de: %.1lf\n ", velmedia);
 
     if (velmedia <= 60) {
-        combustivel = 5 / distancia;
-
-        printf("Consumo medio: %f\n ", combustivel);
-
+        maxkm = (alcmax * 100) / 5; // regra 3 simples para velocidade media menor ou igual a 60 km/h
+        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        puts("Tem alcance suficiente para parar na ultima bomba para abastecer ");
     } else if (velmedia > 60 && velmedia <= 120) {
-        combustivel = 6 / distancia;
-        printf("Consumo medio: %f\n ", combustivel);
-    } else {
-        combustivel = 7 / distancia;
-        printf("Consumo medio: %f\n ", combustivel);
-
+        maxkm = (alcmax * 100) / 6; // regra 3 simples para velocidade media maior que 60 e menor ou igual a 120 km/h
+        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        puts("Tem alcance suficiente para parar na segunda bomba para abastecer ");
+    } else if (velmedia > 120) {
+        maxkm = (alcmax * 100) / 7; // regra 3 simples para velocidade media maior que 120 km/h
+        printf("O maximo de quilometros que conseguira fazer e de %.1f\n  ", maxkm);
+        puts("Impossivel de chegar à primeira bomba. Infelizmente vai ficar encostado a berma ");
     }
-
     return (EXIT_SUCCESS);
 }
 
