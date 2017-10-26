f6f1aa6f28a6b3071acd41e8824eca005456c51b
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-26 15:03:23 +0100

    primeiro commit ex4

---
+++ Ficha 3/Exercicio 4/mainex4.c
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainex4.c
+ * Author: samue
+ *
+ * Created on 26 de Outubro de 2017, 14:54
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define limite 100
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int numero, somatorio = 0;
+
+    for (numero = 1; numero <= limite; ++numero) {
+        printf("%d\n", numero);
+        somatorio += numero;
+    }
+    printf("O somatorio e de: %d\n ", somatorio);
+
+    return (EXIT_SUCCESS);
+}
+
