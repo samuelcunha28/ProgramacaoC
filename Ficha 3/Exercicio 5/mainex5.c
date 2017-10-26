24327985546e875713a532f547f5876ad6fa3ea1
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-10-26 15:17:47 +0100

    primmeiro commit ex5

---
+++ Ficha 3/Exercicio 5/mainex5.c
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainex5.c
+ * Author: samue
+ *
+ * Created on 26 de Outubro de 2017, 15:04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int numero, contador = 0;
+    
+    puts("Introduza o seu numero: ");
+    scanf("%d", &numero);
+    
+    for (contador = numero + 1;contador <= numero + 5; ++contador) {
+        printf("Sucessores: %d\n", contador); 
+    } for (contador = numero - 1;contador >= numero - 5; --contador) {
+        printf("Antecessores: %d\n", contador);
+    }
+    return (EXIT_SUCCESS);
+}
+
