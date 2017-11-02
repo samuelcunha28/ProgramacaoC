f3c4f8332cbc21cf4393977a5e25ab437a3f8a5b
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-11-02 15:33:36 +0000

    sexto commit ex15

--- Ficha 3/Exercicio 15/mainex15.c
+++ Ficha 3/Exercicio 15/mainex15.c
@@ -25,8 +25,8 @@ void limparBufferEntrada() {
  * 
  */
 int main(int argc, char** argv) {
-    int idade, i, contador = 1, casados, f_sal1500, m_18_35_casado, sexom, sexof;
-    char sexo, estado_civil;
+    int idade, i, estado_civil, contador = 1, casados, f_sal1500, m_18_35_casado, sexom, sexof, idademaior = 0, idademenor = 0;
+    char sexo;
     double salario, contador_salario, contador_salariofinal;
 
     while (1) {
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
 
         if (idade >= 16 && idade <= 150) {
             printf("Introduziu a idade: %d\n ", idade);
-            ++idade;
+            //++idade;
         } else if (idade < 0) {
             puts("Introduziu uma idade negativa, o programa ira encerrar ");
             break;
@@ -69,18 +69,18 @@ int main(int argc, char** argv) {
         limparBufferEntrada();
 
         puts("Introduza o seu estado civil (Solteiro - 1, Casado - 2, Divorciado - 3, Viuvo - 4): ");
-        scanf("%c", &estado_civil);
+        scanf("%d", &estado_civil);
 
         if (estado_civil >= 1 && estado_civil <= 4) {
-            if  (estado_civil == 1) {
+            if (estado_civil == 1) {
                 puts("Introduziu o estado civil solteiro ");
-            } else if  (estado_civil == 2) {
+            } else if (estado_civil == 2) {
                 puts("Introduziu o estado civil casado ");
-            } else if  (estado_civil == 3) {
+            } else if (estado_civil == 3) {
                 puts("Introduziu o estado civil divorciado ");
-            } else if  (estado_civil == 4) {
+            } else if (estado_civil == 4) {
                 puts("Introduziu o estado civil viuvo ");
-        } 
+            }
         }
 
         limparBufferEntrada();
@@ -88,6 +88,9 @@ int main(int argc, char** argv) {
         puts("Introduza o seu salario (maior que o salario nacional): ");
         scanf("%lf", &salario);
 
+        
+        // calculo salario
+        
         if (salario > min_salario_nacional) {
             printf("Introduziu o salario %.1lf e este e maior que o salario nacional\n ", salario);
             ++contador_salario;
@@ -95,27 +98,39 @@ int main(int argc, char** argv) {
         } else {
             puts("Introduziu um salario invalido! ");
         }
-        limparBufferEntrada();
 
+        // idade maior e menor
+
+        if (idade > idademaior) {
+            idademaior = idade;
+        } else if (idade < idademenor) {
+            idademenor = idade;
+        }
+
+        if (idade < idademaior) {
+            idademaior = idade;
+        } else if (idade > idademenor) {
+            idademenor = idade;
+        }
+        // feminino com salario ate 1500
+        
+        if (sexo == 'F' && salario <= 1500) {
+            f_sal1500++;
+        }
+
+        // masculino com idade entre 18 e 35 e casados
+        
+        if (sexo == 'M' && idade >= 18 && idade <= 35 && estado_civil == 2) {
+            m_18_35_casado++;
+        }
+        limparBufferEntrada();
         ++contador;
     }
 
+    printf("A idade maior e de: %d\n ", idademaior);
+    printf("A idade menor e de: %d\n ", idademenor);
     printf("A media dos salarios e de %.1lf euros\n ", contador_salariofinal / contador_salario);
-
-    // feminino com salario ate 1500
-   for (i = 1; i <= contador - 1; ++i) {
-        if (sexo == 'F' && salario <= 1500) {
-            f_sal1500 = f_sal1500 + 1;
-        }
-    }
     printf("Existe(m) %d pessoa(s) do sexo feminino com ordenados até 1500 euros\n", f_sal1500);
-
-    // masculino idade entre 18 e 35 casados
-    for (i = 0; i <= contador - 1; ++i) {
-        if (sexo == 'M' && idade >=18 && idade <= 35 && estado_civil == 2) {
-            m_18_35_casado = m_18_35_casado + 1;
-        }
-    }
     printf("Existe(m) %d pessoa(s) do sexo masculino entre os 18 e 35 anos e que tambem sejam casados\n", m_18_35_casado);
 
     return (EXIT_SUCCESS);
