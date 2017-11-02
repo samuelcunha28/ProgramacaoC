dd576fb9be3eac137bb103bb8e10610fb53e9e54
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-11-02 14:34:32 +0000

    quinto commit ex15

--- Ficha 3/Exercicio 15/mainex15.c
+++ Ficha 3/Exercicio 15/mainex15.c
@@ -25,9 +25,9 @@ void limparBufferEntrada() {
  * 
  */
 int main(int argc, char** argv) {
-    int idade, estado_civil, i, contador = 1, contador_salario, contador_salariofinal, casados, f_sal1500, m_18_35_casado;
-    char sexo;
-    double salario;
+    int idade, i, contador = 1, casados, f_sal1500, m_18_35_casado, sexom, sexof;
+    char sexo, estado_civil;
+    double salario, contador_salario, contador_salariofinal;
 
     while (1) {
         printf("Pesquisa numero %d\n ", contador);
@@ -56,10 +56,10 @@ int main(int argc, char** argv) {
 
         if (sexo == 'M') {
             puts("Introduziu o sexo masculino! ");
-            ++sexo;
+            ++sexom;
         } else if (sexo == 'F') {
             puts("Introduziu o sexo feminino! ");
-            ++sexo;
+            ++sexof;
         } else {
             puts("Introduziu um sexo invalido! ");
             break;
@@ -71,20 +71,16 @@ int main(int argc, char** argv) {
         puts("Introduza o seu estado civil (Solteiro - 1, Casado - 2, Divorciado - 3, Viuvo - 4): ");
         scanf("%c", &estado_civil);
 
-        limparBufferEntrada();
-
-        switch (estado_civil) {
-            case '1': puts("Introduziu o estado civil solteiro(a) ");
-                break;
-            case '2': puts("Introduziu o estado civil casado(a) ");
-                ++casados;
-                break;
-            case '3': puts("Introduziu o estado civil divorciado(a) ");
-                break;
-            case '4': puts("Introduziu o estado civil viuvo(a) ");
-                break;
-            default: puts("Introduziu um estado civil incorreto! ");
-                break;
+        if (estado_civil >= 1 && estado_civil <= 4) {
+            if  (estado_civil == 1) {
+                puts("Introduziu o estado civil solteiro ");
+            } else if  (estado_civil == 2) {
+                puts("Introduziu o estado civil casado ");
+            } else if  (estado_civil == 3) {
+                puts("Introduziu o estado civil divorciado ");
+            } else if  (estado_civil == 4) {
+                puts("Introduziu o estado civil viuvo ");
+        } 
         }
 
         limparBufferEntrada();
@@ -95,21 +91,19 @@ int main(int argc, char** argv) {
         if (salario > min_salario_nacional) {
             printf("Introduziu o salario %.1lf e este e maior que o salario nacional\n ", salario);
             ++contador_salario;
-            contador_salariofinal += contador_salario;
+            contador_salariofinal += salario;
         } else {
             puts("Introduziu um salario invalido! ");
         }
-        ++contador_salariofinal;
         limparBufferEntrada();
 
         ++contador;
     }
 
-    printf("A media dos salarios e de %d\n ", contador_salario / contador_salariofinal);
-
+    printf("A media dos salarios e de %.1lf euros\n ", contador_salariofinal / contador_salario);
 
     // feminino com salario ate 1500
-    for (i = 0; i <= contador - 1; ++i) {
+   for (i = 1; i <= contador - 1; ++i) {
         if (sexo == 'F' && salario <= 1500) {
             f_sal1500 = f_sal1500 + 1;
         }
@@ -118,13 +112,11 @@ int main(int argc, char** argv) {
 
     // masculino idade entre 18 e 35 casados
     for (i = 0; i <= contador - 1; ++i) {
-        if (sexo == 'M' && idade <= 35 && idade > 18 && estado_civil == 2) {
+        if (sexo == 'M' && idade >=18 && idade <= 35 && estado_civil == 2) {
             m_18_35_casado = m_18_35_casado + 1;
         }
     }
     printf("Existe(m) %d pessoa(s) do sexo masculino entre os 18 e 35 anos e que tambem sejam casados\n", m_18_35_casado);
-
-
 
     return (EXIT_SUCCESS);
 }
