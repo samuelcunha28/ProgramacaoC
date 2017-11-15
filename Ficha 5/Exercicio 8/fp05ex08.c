25f7f1bb01bea1e9442b884d3a567993f5071d45
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-11-15 20:51:02 +0000

    ex8 completo

--- Ficha 5/Exercicio 8/fp05ex08.c
+++ Ficha 5/Exercicio 8/fp05ex08.c
@@ -1,49 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: samue
  *
- * Created on 15 de Novembro de 2017, 19:15
+ * Created on 15 de Novembro de 2017, 19:52
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 1000
+#define TAMANHO_ARRAY 1000
+
+// ficha 5 ex 8
 
 int main(int argc, char** argv) {
-    int i = 0, array[MAX];
+    int array[TAMANHO_ARRAY], i = 0, num;
 
-    for (i = 0; i < MAX; ++i) {
-        array[i] = i;
+    puts("Insira um numero (que nao seja maior que 1000):");
+    scanf("%d", &num);
+
+    if (num >= 0 && num <= TAMANHO_ARRAY) {
+        for (i = 0; i < num; ++i) {
+            array[i] = i;
+        }
+
+        for (i = 0; i < num; ++i) {
+            array[i] = i;
+        }
+        //multiplos de 2
+        for (i = 4; i < num; i = i + 2) {
+            array[i] = 0;
+        }
+        //multiplos de 3
+        for (i = 6; i < num; i = i + 3) {
+            array[i] = 0;
+        }
+        //multiplos de 5
+        for (i = 10; i < num; i = i + 5) {
+            array[i] = 0;
+        }
+        //multiplos de 7
+        for (i = 14; i < num; i = i + 7) {
+            array[i] = 0;
+        }
+    } else {
+        puts("Introduziu um numero maior que o limite indicado!!");
     }
-    //multiplos de 2
-    for (i = 4; i < MAX; i = i + 2) {
-        array[i] = 0;
-    }
-    //multiplos de 3
-    for (i = 6; i < MAX; i = i + 3) {
-        array[i] = 0;
-    }
-    //multiplos de 5
-    for (i = 10; i < 1000; i = i + 5) {
-        array[i] = 0;
-    }
-    //multiplos de 7
-    for (i = 14; i < 1000; i = i + 7) {
-        array[i] = 0;
-    }
-    // imprimir array
-    for (i = 2; i < MAX; ++i) {
+
+    for (i = 2; i < num; ++i) {
         if (array[i] != 0) {
             printf("%d\n ", array[i]);
         }
     }
-
     return (EXIT_SUCCESS);
 }
+
