031e532563deb42b5ebb37c42b371bc6d642f580
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-11-21 20:56:23 +0000

    primeiro commit ex7

---
+++ Ficha 6/Exercicio 7/build/Debug/Cygwin-Windows/fp06ex07.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/fp06ex07.o: fp06ex07.c utils.h
+
+utils.h:
---
+++ Ficha 6/Exercicio 7/fp06ex07.c
@@ -0,0 +1,40 @@
+/* 
+ * File:   fp06ex07.c
+ * Author: samue
+ * FICHA 6
+ * Created on 21 de Novembro de 2017, 20:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+#define MAX_CARACTERES 25
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char frase_palavra[MAX_CARACTERES];
+    int i, contador = 0;
+    
+    
+    for (i = 0; i < MAX_CARACTERES; ++i) {
+        frase_palavra[i] = ' ';
+    }
+    
+    puts("Insira o seu texto: ");
+    lerString(frase_palavra, MAX_CARACTERES);
+    
+    for (i = 0; i < MAX_CARACTERES; ++i) {
+        if (frase_palavra[i] == ' '){
+            continue;
+        }
+        else {
+            ++contador;
+        }
+    }
+    
+    printf("Numero de caracteres sem contar os espacos: %d\n", contador - 2);
+    
+    return (EXIT_SUCCESS);
+}
