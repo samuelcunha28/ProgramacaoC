2282bfc68d7d6fdb0e47e7a506935e5b77b27fc9
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2017-11-25 17:45:02 +0000

    ex6 completo

--- Ficha 7/Exercicio 6/fp07ex06.c
+++ Ficha 7/Exercicio 6/fp07ex06.c
@@ -1,8 +1,8 @@
 /* 
- * File:   fp07ex06.c
+ * File:   main.c
  * Author: samue
- * FICHA 7
- * Created on 23 de Novembro de 2017, 22:19
+ *
+ * Created on 25 de Novembro de 2017, 16:23
  */
 
 #include <stdio.h>
@@ -10,16 +10,15 @@
 
 #define MAX 10
 
-int num, i = 0, j = 0;
-
 int dimensao() {
+    int num;
 
     while (1) {
-        printf("Insira o tamanho da matriz\n ");
+        printf("Insira o tamanho: ");
         scanf("%d", &num);
 
         if (num > MAX) {
-            printf("Valor Invalido. Tente novamente\n ");
+            printf("Valor maior que o permitido. Tente novamente ");
         } else {
             break;
         }
@@ -27,77 +26,67 @@ int dimensao() {
     return num;
 }
 
-void preencherMatrizA(int matrizA[][num]) {
+int main(int argc, char** argv) {
+    int i = 0, j = 0, k = 0, soma = 0;
+    int LinA, LinB, ColA, ColB;
+    int matrizA[MAX][MAX], matrizB[MAX][MAX], matrizC[MAX][MAX];
+
+    puts("Matriz A ");
+    puts("Quantas linhas tem a matriz A?");
+    LinA = dimensao();
+    puts("Quantas colunas tem a matriz A?");
+    ColA = dimensao();
+
+    puts("Matriz B ");
+    puts("Quantas linhas tem a matriz B?");
+    LinB = dimensao();
+    puts("Quantas colunas tem a matriz B?");
+    ColB = dimensao();
+
+    while (1) { // para se efetuar o calculo o numero de colunas da matriz A tem de ser igual ao numero de linhas da matriz B
+        if (ColA == LinB) {
+            break;
+        } else {
+             puts("Impossivel multiplicar a matriz C porque o numero de colunas da matriz A nao e igual ao numero de linhas da matriz B! ");
+        } 
+    }
 
     puts("Preencha a Matriz A: ");
-    for (i = 0; i < num; ++i) {
-        for (j = 0; j < num; ++j) {
+    for (i = 0; i < LinA; ++i) {
+        for (j = 0; j < ColA; ++j) {
             printf("Introduza o seu valor para [%d] [%d]: ", i, j);
             scanf("%d", &matrizA[i][j]);
         }
         puts(" ");
     }
-}
 
-void escreverMatrizA(int matrizA[][num]) {
-
-    puts("Matriz A");
-    for (i = 0; i < num; ++i) {
-        for (j = 0; j < num; ++j) {
-            printf(" %d", matrizA[i][j]);
-        }
-        puts(" ");
-    }
-    puts(" ");
-}
-
-void preencherMatrizB(int matrizB[][num]) {
-    
-    puts("Preencha a Matriz B: ");
-    for (i = 0; i < num; ++i) {
-        for (j = 0; j < num; ++j) {
+    puts("Preencha a Matriz B");
+    for (i = 0; i < LinB; ++i) {
+        for (j = 0; j < ColB; ++j) {
             printf("Introduza o seu valor para [%d] [%d]: ", i, j);
             scanf("%d", &matrizB[i][j]);
         }
         puts(" ");
     }
-}
 
-void escreverMatrizB(int matrizB[][num]) {
 
-    puts("Matriz B");
-    for (i = 0; i < num; ++i) {
-        for (j = 0; j < num; ++j) {
-            printf(" %d", matrizB[i][j]);
+    for (i = 0; i < LinA; ++i) { // calculo da matriz C
+        for (j = 0; j < ColB; ++j) {
+            matrizC[i][j] = 0;
+            for (k = 0; k < LinB; ++k) {
+                soma += matrizA[i][k] * matrizB [k][j];
+            }
+            matrizC[i][j] = soma;
+        }
+    }
+
+
+    puts("Matriz C"); // impressao da matriz resultante
+    for (i = 0; i < LinA; ++i) {
+        for (j = 0; j < ColB; ++j) {
+            printf(" %d", matrizC[i][j]);
         }
         puts(" ");
     }
-    puts(" ");
-}
-
-void escreverMatrizC(int matrizA[][num], int matrizB[][num]) {
-    
-    puts("Matriz C");
-    for (i = 0; i < num; ++i) {
-        for (j = 0; j < num; ++j) {
-            printf(" %d", matrizA[i][j] * matrizB[i][j]);
-        }
-        puts(" ");
-    }
-    puts(" ");
-}
-   
-
-/* Exercicio 6 Ficha 7 */
-int main(int argc, char** argv) {
-    int matriz[num][num];
-
-    dimensao(num);
-    preencherMatrizA(matriz); // chamar procedimento para preencher a matriz A
-    escreverMatrizA(matriz); // chamar procedimento para escrever a matriz A
-    preencherMatrizB(matriz); // chamar procedimento para preencher a matriz B
-    escreverMatrizB(matriz); // chamar procedimento para escrever a matriz B
-    escreverMatrizC(matriz, matriz); // chamar procedimento para escrever a matriz C resultante da A e B
-
     return (EXIT_SUCCESS);
 }
